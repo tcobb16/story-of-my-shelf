ec8c189e1c9e209cd840e62d8b17b39351e935a8
Tessa Cobb <tcobb2162@gmail.com>
2022-12-08 11:56:58 -0800

    started base html doc and created initial databases in models.py

---
+++ app.py
@@ -0,0 +1,101 @@
+import os
+
+from flask import Flask, render_template, request, flash, redirect, session, g
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+from forms import AddUserForm, LoginForm
+from models import db, connect_db, Users, Books, Authors
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.before_request
+def add_user_to_g():
+    """Add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = Users.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup."""
+
+    form = AddUserForm()
+
+    if form.validate_on_submit():
+        try:
+            user = Users.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+                image_url=form.image_url.data or Users.image_url.default.arg,
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", 'danger')
+            return render_template('users/signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('users/signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = Users.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('users/login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+    flash(f"Goodbye!", "success")
+    return redirect("/login")
---
+++ models.py
@@ -0,0 +1,149 @@
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+
+class Users(db.Model):
+    """User of app"""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    profile_pic_url = db.Column(
+        db.Text,
+        default="/static/images/default-profile-pic.png",
+    )
+
+    favorites = db.relationship('Books', secondary=favorites, lazy='subquery', backref=db.backref('users', lazy=True))
+
+    to_be_read = db.relationship('Books', secondary=to_be_read, lazy='subquery', backref=db.backref('users', lazy=True))
+
+    read = db.relationship('Books', secondary=read, lazy='subquery', backref=db.backref('users', lazy=True))
+
+    @classmethod
+    def signup(cls, username, email, password, profile_pic_url):
+        """Sign up user.
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = Users(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            profile_pic_url=profile_pic_url,
+        )
+
+        db.session.add(user)
+        return user
+        
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+
+class Books(db.Model):
+    """Individual books"""
+
+    __tablename__ = 'books'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    title = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    author = db.Column(
+        db.Integer,
+        db.ForeignKey('authors.id'),
+        unique=True,
+    )
+
+    genre = db.Column(
+        db.Text,
+    )
+
+    cover_img = db.Column(
+        db.Text,
+    )
+
+
+class Authors(db.Model):
+    """Authors"""
+
+    __tablename__ = 'authors'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+
+favorites = db.Table(
+    'favorites', 
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id', primary_key=True)),
+    db.Column('book_id', db.Integer, db.ForeignKey('books.id', primary_key=True)),
+)
+
+
+to_be_read = db.Table(
+    'to_be_read',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id', primary_key=True)),
+    db.Column('book_id', db.Integer, db.ForeignKey('books.id', primary_key=True)),
+)
+
+
+read = db.Table(
+    'read',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id', primary_key=True)),
+    db.Column('book_id', db.Integer, db.ForeignKey('books.id', primary_key=True)),
+)
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app."""
+
+    db.app = app
+    db.init_app(app)
+
+
+    
